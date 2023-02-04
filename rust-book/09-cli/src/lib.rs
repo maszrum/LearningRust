8fa3e64c5b05ea56706784f9fe1493f8d8b59db1
maszrum <grzyb3k@hotmail.com>
2023-02-04 18:32:38 +0100

    Use iterators in cli crate

--- rust-book/09-cli/src/lib.rs
+++ rust-book/09-cli/src/lib.rs
@@ -8,13 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() != 3 {
-            return Err("invalid number of arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file path")
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
